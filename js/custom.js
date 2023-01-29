d9017610b3a2c35ac135df18cf1764907867e47d
Aman Hussain <v-hussaina@microsoft.com>
2023-01-29 23:45:01 +0530

    changes

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,8 @@
 $(document).ready(function(){
-    $('.partner-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        autoplaySpeed: 2000,
-        autoplay: true,
-        pauseOnHover: false,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-        ]
-    });
+    $(".career-apply").click(function(){
+        setTimeout(() => {
+            $(".contact-form").removeClass("active");
+        }, 1000);
+        $(".contact-form").addClass("active");
+    })
 })
